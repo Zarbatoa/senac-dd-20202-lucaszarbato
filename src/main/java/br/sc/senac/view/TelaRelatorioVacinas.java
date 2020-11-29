1cd6df288b6cd12846b9a65e11f10b601535f8f5
Lucas Pereira Zarbato <lpzarbato@gmail.com>
2020-11-29 04:31:47 -0300

    Tela de relatório de vacinas replanejada

--- src/main/java/br/sc/senac/view/TelaRelatorioVacinas.java
+++ src/main/java/br/sc/senac/view/TelaRelatorioVacinas.java
@@ -20,16 +20,16 @@ import com.github.lgooddatepicker.components.DatePickerSettings;
 
 import br.sc.senac.model.vo.Pessoa;
 import net.miginfocom.swing.MigLayout;
+import javax.swing.ImageIcon;
 
 @SuppressWarnings({"serial", "rawtypes", "unchecked"})
 public class TelaRelatorioVacinas extends JFrame {
 
 	private JPanel contentPane;
 	private JComboBox cbRelatorioVacinas;
-	private JTable table;
+	private JTable tableResultados;
 	private JTextField txtUsuario;
-	private DatePicker dataInicio;
-	private DatePicker dataFinal;
+	private DatePicker dpDataFiltro;
 	
 	/**
 	 * Launch the application.
@@ -58,7 +58,7 @@ public class TelaRelatorioVacinas extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[grow][27.00,grow][grow][][91.00][41.00][142.00,grow][42.00]", "[][][][][][][][][][][99.00][][][]"));
+		contentPane.setLayout(new MigLayout("", "[grow][27.00,grow][grow][][91.00][41.00][142.00,grow][42.00]", "[][][][][][][][][][][][]"));
 		
 		JLabel lbbRelatorioVacinas = new JLabel("Relat\u00F3rio de Vacinas");
 		lbbRelatorioVacinas.setFont(new Font("Tahoma", Font.BOLD, 11));
@@ -67,54 +67,48 @@ public class TelaRelatorioVacinas extends JFrame {
 		String[] opcoesSexo = {Pessoa.SEXO_MASCULINO, Pessoa.SEXO_FEMININO}; 
 		
 		cbRelatorioVacinas = new JComboBox(opcoesSexo);
-		cbRelatorioVacinas.setModel(new DefaultComboBoxModel(new String[] {"Total de Vacinas dos Usu\u00E1rios", "Total de Vacinas por Usu\u00E1rio", "Total de Vacinas dos Usu\u00E1rios por Per\u00EDodo", "Total de Vacinas por Usu\u00E1rio e Per\u00EDodo", "", "", ""}));
+		cbRelatorioVacinas.setModel(new DefaultComboBoxModel(new String[] {"Total de Vacinas por Pesquisador", "==================", "Total de Vacinas por Pa\u00EDs de origem", "Total de Vacinas por Pa\u00EDs de origem a partir de uma data", "Total de Vacinas por Est\u00E1gio de Pesquisa", "Total de Vacinas por Est\u00E1gio de Pesquisa a partir de uma data"}));
 		cbRelatorioVacinas.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		contentPane.add(cbRelatorioVacinas, "cell 0 2 8 1,growx");
 		
-		JLabel lblUsurio = new JLabel("Usu\u00E1rio:");
-		lblUsurio.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		contentPane.add(lblUsurio, "cell 0 4,alignx trailing");
+		JLabel lblPesquisador = new JLabel("Pesquisador:");
+		lblPesquisador.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		contentPane.add(lblPesquisador, "cell 0 4,alignx trailing");
 		
 		txtUsuario = new JTextField();
 		txtUsuario.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		contentPane.add(txtUsuario, "cell 1 4 2 1,growx");
 		txtUsuario.setColumns(10);
 		
-		JLabel lblDataInicio = new JLabel("De:");
-		lblDataInicio.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		contentPane.add(lblDataInicio, "cell 0 6,alignx trailing");
-		
 		DatePickerSettings dateSettings = new DatePickerSettings();
 		dateSettings.setAllowKeyboardEditing(false);
-		dataInicio = new DatePicker(dateSettings);
-		contentPane.add(dataInicio,"cell 1 6 2 1,growx");
 		
 		//TODO pensar sobre um botão cancelar ou voltar
 		final JFrame janelaAtual = this;
 		
-		JLabel lblDataFinal = new JLabel("At\u00E9:");
-		lblDataFinal.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		contentPane.add(lblDataFinal, "cell 5 6,alignx trailing");
-		
 		DatePickerSettings dateSettings2 = new DatePickerSettings();
 		dateSettings2.setAllowKeyboardEditing(false);
-		dataFinal = new DatePicker(dateSettings2);
-		contentPane.add(dataFinal,"cell 6 6 2 1,growx");
+		
+		JLabel lblData = new JLabel("Data:");
+		lblData.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		contentPane.add(lblData, "cell 5 4,alignx trailing");
+		dpDataFiltro = new DatePicker(dateSettings2);
+		contentPane.add(dpDataFiltro,"cell 6 4 2 1,growx");
 		
 		//JFormattedTextField ftfDataFinal = new JFormattedTextField();
 		//contentPane.add(ftfDataFinal, "cell 6 6 2 1,growx");
 		
 		JButton btnGerarRelatorio = new JButton("Gerar Relat\u00F3rio");
 		btnGerarRelatorio.setFont(new Font("Tahoma", Font.BOLD, 11));
-		contentPane.add(btnGerarRelatorio, "cell 2 8,alignx right");
+		contentPane.add(btnGerarRelatorio, "cell 2 6,alignx right");
 		
-		JButton btnGerarPdf = new JButton("Gerar Pdf");
-		btnGerarPdf.setFont(new Font("Tahoma", Font.BOLD, 11));
-		contentPane.add(btnGerarPdf, "cell 6 8");
+		JButton btnGerarExel = new JButton("exel");
+		btnGerarExel.setFont(new Font("Tahoma", Font.BOLD, 11));
+		contentPane.add(btnGerarExel, "cell 6 6");
 		
-		table = new JTable();
-		table.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		table.setModel(new DefaultTableModel(
+		tableResultados = new JTable();
+		tableResultados.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		tableResultados.setModel(new DefaultTableModel(
 			new Object[][] {
 				{"Usu\u00E1rio", "Data Inicial", "Data Final", "País de Origem", "Instituição", "Total de Vacinas"},
 				{null, null, null, null, null, null},
@@ -127,23 +121,23 @@ public class TelaRelatorioVacinas extends JFrame {
 				"New column", "New column", "New column", "New column", "New column", "New column"
 			}
 		));
-		contentPane.add(table, "cell 0 10 8 1,grow");
+		contentPane.add(tableResultados, "cell 0 8 8 1,grow");
 		
 		JButton buttonPagAnterior = new JButton("< Anterior");
 		buttonPagAnterior.setFont(new Font("Tahoma", Font.BOLD, 11));
-		contentPane.add(buttonPagAnterior, "cell 2 12");
+		contentPane.add(buttonPagAnterior, "cell 2 10");
 		
 		JLabel lblPagAtual = new JLabel("1");
 		lblPagAtual.setFont(new Font("Tahoma", Font.BOLD, 11));
-		contentPane.add(lblPagAtual, "cell 4 12,alignx center");
+		contentPane.add(lblPagAtual, "cell 4 10,alignx center");
 		
 		JButton btnPagProxima = new JButton("Pr\u00F3ximo >");
 		btnPagProxima.setFont(new Font("Tahoma", Font.BOLD, 11));
-		contentPane.add(btnPagProxima, "cell 6 12");
+		contentPane.add(btnPagProxima, "cell 6 10");
 		
 		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.setFont(new Font("Tahoma", Font.BOLD, 11));
-		contentPane.add(btnVoltar, "cell 4 13,alignx center");
+		contentPane.add(btnVoltar, "cell 4 11,alignx center");
 	}
 
 }
